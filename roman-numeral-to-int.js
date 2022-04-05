f827637849aa9ea386061c3abe21065d3229a641
Shirley Li <lishirley@me.com>
2022-04-04 17:13:00 -0700

    added directions

--- roman-numeral-to-int.js
+++ roman-numeral-to-int.js
@@ -1,3 +1,47 @@
+/*
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+*/
+
+
 s1 = "III"
 expected1 = 3
 
@@ -12,7 +56,7 @@ expected3 = 1994
  * @return {number}
  */
 
-var romanToInt = function(s) {
+var romanToInt = function (s) {
     let sum = 0;
     const hashMap = {
         "I": 1,
@@ -23,10 +67,10 @@ var romanToInt = function(s) {
         "D": 500,
         "M": 1000
     }
-    for(i = 0; i < s.length; i++){
+    for (i = 0; i < s.length; i++) {
         let currentNumeral = s.charAt(i);
-        let nextNumeral = s.charAt(i+1);
-        if(hashMap[currentNumeral] < hashMap[nextNumeral]) {
+        let nextNumeral = s.charAt(i + 1);
+        if (hashMap[currentNumeral] < hashMap[nextNumeral]) {
             sum += (hashMap[nextNumeral] - hashMap[currentNumeral]);
             i++;
         } else {
